ff436d6d3a5bc83ff6889749d2f387c39063dfc5
underethsea <underthesea.pool@gmail.com>
2021-10-14 22:26:50 -0700

    add-price

--- src/views/TotalValueLocked.jsx
+++ src/views/TotalValueLocked.jsx
@@ -1,74 +1,11 @@
 import React, { useState } from "react";
-import { formatEther } from "ethers/lib/utils";
-import { useExternalContractLoader, useContractReader, useBalance } from "../hooks";
 import { useParams } from "react-router-dom";
-import { FARMS, STAKING_POOL_ADDRESSES } from "../constants";
-import FarmUI from "./FarmUI";
-import FarmNYANETHUI from "./FarmNYANETHUI";
 import { numberWithCommas } from "../helpers";
 
-import WETHTokenAddress from "../contracts/WETHToken.address";
-import ERC20Abi from "../contracts/ERC20.abi";
-
-import NyanTokenAddress from "../contracts/NyanToken.address";
-import NyanTokenAbi from "../contracts/NyanToken.abi";
-
-/* arbi's stategies */
-import NyanStrategyAddress from "../contracts/NyanStrategy.address";
-import NyanStrategyAbi from "../contracts/NyanStrategy.abi";
-import NyanETHSushiLPAddress from "../contracts/NyanETHSushiLP.address";
-import SushiLPAbi from "../contracts/SushiLP.abi";
-import NyanETHStrategyAddress from "../contracts/NyanETHStrategy.address";
-import CarbonStrategyAddress from "../contracts/CarbonStrategy.address";
-import PongStrategyAddress from "../contracts/PongStrategy.address";
-import PonzuStrategyAddress from "../contracts/PonzuStrategy.address";
-import ApeStrategyAddress from "../contracts/ApeStrategy.address";
-import USDCETHStrategyAddress from "../contracts/USDCETHStrategy.address";
-
-
-export default function TotalValueLocked({
-  ethUSDPrice,
-  nyanETHPrice,
-  setPurposeEvents,
-  address,
-  mainnetProvider,
-  userSigner,
-  localProvider,
-  yourLocalBalance,
-  price,
-  tx,
-  writeContracts,
-  injectedProvider,
-}) {
+export default function TotalValueLocked() {
   const [loadedRemoteTVL, setLoadedRemoteTVL] = React.useState(false);
   const [remoteTVL, setRemoteTVL] = React.useState("0");
-
-  const wethTokenInstance = useExternalContractLoader(injectedProvider, WETHTokenAddress, ERC20Abi);
-
-  /* NYAN strategy */
-  const nyanStrategyInstance = useExternalContractLoader(injectedProvider, NyanStrategyAddress, NyanStrategyAbi);
-  const nyanStrategyTotalDeposits = useContractReader({ NyanStrategy: nyanStrategyInstance }, "NyanStrategy", "totalDeposits");
-
-  /* NYAN/ETH strategy */
-  const nyanETHStrategyInstance = useExternalContractLoader(injectedProvider, NyanETHStrategyAddress, NyanStrategyAbi); 
-  const nyanETHStrategyTotalDeposits = useContractReader({ NYANETHStrategy: nyanETHStrategyInstance }, "NYANETHStrategy", "totalDeposits");
-  /* query WETH token contract for LP balance */
-  const nyanETHStrategyWethLPBalance = useContractReader({ ERC20: wethTokenInstance }, "ERC20", "balanceOf", [NyanETHSushiLPAddress]);
-  /* get sushi lp total supply */
-  const nyanETHStrategyLpInstance = useExternalContractLoader(injectedProvider, NyanETHSushiLPAddress, SushiLPAbi); 
-  const nyanETHStrategyLpTotalSupply = useContractReader({ SLP: nyanETHStrategyLpInstance }, "SLP", "totalSupply");
-
-  /* Sushi USDC/ETH strategy */
-  const usdcETHStrategyInstance = useExternalContractLoader(injectedProvider, USDCETHStrategyAddress, NyanStrategyAbi);
-  const usdcETHStrategyTotalDeposits = useContractReader({ USDCETHStrategy: usdcETHStrategyInstance }, "USDCETHStrategy", "totalDeposits");
-  /* TODO */
-
-  /* MIM/ETH strategy */
-  /* TODO */
-
-  /* MAGIC/USDC strategy */
-  /* TODO */
-
+  const [remotePrice, setRemotePrice] = React.useState("0");
 
   async function tryGetFullTVL() {
     try {
@@ -77,6 +14,7 @@ export default function TotalValueLocked({
       console.log(`full tvl data ${data}`);
 
       setRemoteTVL(data.tvl);
+      setRemotePrice(data.arbisPrice);
       setLoadedRemoteTVL(true);
     } catch(e) {
       console.log(e);
@@ -89,23 +27,13 @@ export default function TotalValueLocked({
 
   }, []);
 
-  function tvl() {
-    const nyanStrategyTVL = nyanETHPrice * parseFloat(formatEther(nyanStrategyTotalDeposits));
-    const nyanETHRatio = parseFloat(formatEther(nyanETHStrategyTotalDeposits)) / parseFloat(formatEther(nyanETHStrategyLpTotalSupply));
-    const nyanETHStrategyTVL = nyanETHRatio * parseFloat(formatEther(nyanETHStrategyWethLPBalance)); 
-    const total = (nyanStrategyTVL + nyanETHStrategyTVL) * ethUSDPrice;
-    return total.toFixed(2);
-  } 
-
-  return (
-    <div className="total-value-locked">
-      Total value staked: 
-      {
-        (nyanStrategyTotalDeposits && 
-          nyanETHStrategyTotalDeposits && nyanETHStrategyWethLPBalance && nyanETHStrategyLpTotalSupply && !isNaN(tvl()) && tvl() > 1) 
-          ? 
-          (` $${numberWithCommas(loadedRemoteTVL ? remoteTVL : tvl())}+`) 
-          : 
+  return ( <div className="total-value-locked"> { loadedRemoteTVL ? <span>
+   
+      <span className="tvl-center">Total value staked ${numberWithCommas(remoteTVL)}</span>
+      <span className="price-right">${remotePrice}</span>
+      
+      </span> : 
+      
           (
             <span className="wave"> 
               &nbsp;$
@@ -120,7 +48,7 @@ export default function TotalValueLocked({
               <span style={{"--i":10}}>🍔</span>
             </span>
           )
-      }
+          }
     </div>
   );
 }
