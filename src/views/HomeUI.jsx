3b1d6660960bba2439cca825d9ab7185e2a9e7ee
luffyowls <90647699+luffyowls@users.noreply.github.com>
2021-11-16 00:36:23 +0000

    Clean up landing page

--- src/views/HomeUI.jsx
+++ src/views/HomeUI.jsx
@@ -116,49 +116,34 @@ export default function CreateUI({
         </div>
       ) : (
         <>
-          <p className="warning" style={{"margin": "20px 0"}}>Warning: high risk of extreme volatility of underlying assets and smart contracts. All contracts have not been formally audited.</p> 
-          <hr></hr>
           <div style={{width: "60%", textAlign: "left", margin: "auto", marginTop: "50px"}}>
-           
-           <p style={{textAlign: "center"}}><b>Check out the Arbi's foodcourt bringing fun new products and forks of faves to Arbitrum!</b></p>
-           
-           <ImageMap
-	          className="usage-map"
-	          src={img}
-          	map={mapArea}
-	          onMapClick={onMapClick}
-          />
-           
-           {/* <img src="/arbis-food-court-v3.png" style={{width: "100%"}}/> */}
-           
-           
-           <p><a href="https://zerotwohm.finance">ZeroTwOhm</a> - The first Ohmlike on Arbitrum</p>
-           <p><a href="https://Arbicheems.finance">ArbiCheems</a>+CheemsHole - A simple (highly deflationary) meme coin and a winner take all degen gambling game (new Blackhole but now more balanced for all sizes of playes then previous versions)  </p>
-           
-           
-            <h3>About</h3>
-            <p>See our new documentation: <a href="https://arbisfinance.gitbook.io/food-court/">here</a></p>
-          {/*   <p>Arbi’s Finance is the premier autocompounder on the Arbitrum network. Deposit your tokens in our vaults and earn more yield while saving time.</p>
-            <h1>FAQ</h1>
-            <h2>How does Arbi’s Finance work?</h2>
-            <p>Arbi’s Finance auto-compounds yield farming rewards. Users deposit their tokens into vaults. Vaults earn yield in reward tokens, such as $SUSHI, which is then reinvested in the vault, compounding everyone’s vault deposits.</p>
-            <h2>What are the fees?</h2>
-            <p>Our fees are the tastiest on all of Arbitrum. The default performance fees are 3.5% for our treasury and 0.5% for the compounder. <em>Fees are applied only on the amount being compounded</em>, not TVL. All vaults have at most a 0.1% withdrawal fee.</p>
-            <h2>Why should I use Arbi’s Finance?</h2>
-            <p>Our protocol saves you time and makes you more money by compounding your position for you.</p>            
-            <h2>What is the yield and where does it come from?</h2>
-            <p>Vaults receive yield in various tokens depending on the token and farm type. Our service is an auto-compounding service which automates the process of compounding your position to earn you extra yield.</p>
-           <h2>What is APY and why is it so high?</h2>
-            <p>APY is compounded APR. APR is generally what is shown on a project’s homepage. APY factors in the frequency at which the interest is compounded over a year-long period, resulting in a much higher figure.</p>
-           <h2>What are the risks associated?</h2>
-            <p>Arbi’s Finance pools come with inherent risks, including but not limited to:</p>
-            <ul>
-<li>Smart contract risk -- all of our contracts are unaudited and many of the underlying smart contracts from other decentralized applications and projects which our vaults are built on may be unaudited.</li>
-<li>Scam or rug-pull risk of deposited tokens -- we are not associated with any of the projects that we list unless stated otherwise. The tokenomics, security practices, and code regarding tokens from projects which we provide products for is outside of our control.</li>
-<li>Impermanent loss (in the case of multi-asset liquidity pools) – price variation and divergence of pooled assets leading to losses for liquidity providers.</li>
-          </ul> */}
-<p>Please use our products responsibility, and never invest more than you can afford to lose. Arbi’s Finance does not guarantee compensation for any of our users in the event of exploits, hacks, and other unforeseeable negative events.</p>
+            
+            <h2>Arbi's Finance</h2>
+            <p>Arbi’s Finance is the creator of the Arbi's food court, an ecosystem of burger-themed DeFi projects built on the Arbitrum network.</p>
+            <ImageMap
+              className="usage-map"
+              src={img}
+              map={mapArea}
+              onMapClick={onMapClick}
+              style={{marginBottom: "50px"}}
+            />
+            
+            {/* <img src="/arbis-food-court-v3.png" style={{width: "100%"}}/> */}
+            
+            <h2>Projects</h2>
+            <p><a href="#">Arbi's</a> - Autocompounders for most yield opportunities on Arbitrum. Stake now to maximize yields and save time.</p>
+            <p><a href="https://zerotwohm.finance">ZeroTwOhm</a> - The first decentralized reserve currency protocol natively built on the Arbitrum One Network.</p>
+            <p><a href="https://arbicheems.finance">ArbiCheems</a> - A simple, highly deflationary, meme coin. Stake your assets and receive the CHEEMS token.</p>
+            <p><a href="https://cheemshole.arbicheems.finance">CheemsHole</a> - A winner takes all degen gambling game for CHEEMS, a revised version of Blackhole.</p>
+            
+            <h2>Documentation</h2>
+            <p>You can read more about the food court and our tokenomics in our Gitbook <a href="https://arbisfinance.gitbook.io/food-court/">here</a>.</p>
+
+            <h2>Disclaimer</h2>
+            <p>Please use our products responsibility, and never invest more than you can afford to lose. Arbi’s Finance does not guarantee compensation for any of our users in the event of exploits, hacks, and other unforeseeable negative events.</p>
           </div>
+          <hr></hr>
+          <p className="warning" style={{"margin": "20px 0"}}>Warning: high risk of extreme volatility of underlying assets and smart contracts. All contracts have not been formally audited.</p> 
         </>
       )}
     </div>
